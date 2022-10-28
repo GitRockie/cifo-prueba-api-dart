03250d58af7ff1efee6ffeeb0f3ab838adcf89af
Alex Escobar Ros <76057139+GitRockie@users.noreply.github.com>
2022-10-28 09:54:40 +0200

    Project structure create on main

---
+++ rick_and_morthy_api/android/app/src/main/kotlin/com/example/rick_and_morthy_api/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.rick_and_morthy_api
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ rick_and_morthy_api/lib/pages/pages.dart
@@ -0,0 +1,1 @@
+export 'package:rick_and_morthy_api/pages/home_page.dart';
